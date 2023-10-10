c60d33a6a0714132b76fbfccbd7bf288d2d3dc29
Ilya <ilya@ubuntu22.04.myguest.virtualbox.org>
2023-10-10 19:56:17 +0300

    "add EventListebers"

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('\u0021');
+/* eslint-disable no-undef */
+
+function upp() {
+  document.querySelectorAll('ul li')
+    .forEach((x) => x.textContent = x.textContent.toUpperCase());
+  document.querySelectorAll('ul li')[1].style.color = 'blue';
+
+  setTimeout(() => {
+    document.querySelector('button').dispatchEvent(new MouseEvent('click'));
+}, 5000);
+}
